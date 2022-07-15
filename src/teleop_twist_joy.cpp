ebcb65c71202d6fea5030e4abddeb3c8ad81e7b9
Khrystyna Rud <khrystynarud@outlook.com>
2022-07-15 15:11:33 +0200

    Add rotation deadzone

--- src/teleop_twist_joy.cpp
+++ src/teleop_twist_joy.cpp
@@ -47,8 +47,8 @@ struct TeleopTwistJoy::Impl
 
   int deadman_switch;
 
-  bool require_turbo_button;
-  int enable_turbo_button;
+  // bool require_turbo_button;
+  // int enable_turbo_button;
 
   std::map<std::string, double> min_linear_map;
   std::map<std::string, double> max_linear_map;
@@ -57,8 +57,9 @@ struct TeleopTwistJoy::Impl
   std::map<std::string, double> max_angular_map;
 
   std::map<std::string, double> deadzone_linear_map;
-
   std::map<std::string, double> deadzone_angular_map;
+
+  double deadzone_rotation;
 
   std::map<std::string, int> axis_linear_map;
 
@@ -81,8 +82,8 @@ TeleopTwistJoy::TeleopTwistJoy(ros::NodeHandle* nh, ros::NodeHandle* nh_param)
 
   nh_param->param<int>("deadman_switch", pimpl_->deadman_switch, -1);
 
-  nh_param->param<bool>("require_turbo_button", pimpl_->require_turbo_button, true);
-  nh_param->param<int>("enable_turbo_button", pimpl_->enable_turbo_button, -1);
+  // nh_param->param<bool>("require_turbo_button", pimpl_->require_turbo_button, true);
+  // nh_param->param<int>("enable_turbo_button", pimpl_->enable_turbo_button, -1);
 
   if (!nh_param->getParam("axis_linear", pimpl_->axis_linear_map))
   {
@@ -110,20 +111,25 @@ TeleopTwistJoy::TeleopTwistJoy(ros::NodeHandle* nh, ros::NodeHandle* nh_param)
     }
   }
 
-  ROS_INFO_NAMED("TeleopTwistJoy", "Teleop require turbo button %i.", pimpl_->require_turbo_button);
-  ROS_INFO_NAMED("TeleopTwistJoy", "Teleop deadman_switch %i.", pimpl_->deadman_switch);
-  ROS_INFO_COND_NAMED(pimpl_->enable_turbo_button >= 0, "TeleopTwistJoy", "Turbo on button %i.", pimpl_->enable_turbo_button);
+  if (!nh_param->getParam("deadzone_rotation", pimpl_->deadzone_rotation))
+  {
+    nh_param->param<double>("deadzone_rotation", pimpl_->deadzone_rotation, 0.0);
+  }
+
+  // ROS_INFO_NAMED("TeleopTwistJoy", "Teleop require turbo button %i.", pimpl_->require_turbo_button);
+  // ROS_INFO_COND_NAMED(pimpl_->enable_turbo_button >= 0, "TeleopTwistJoy", "Turbo on button %i.", pimpl_->enable_turbo_button);
+  ROS_INFO_COND_NAMED(pimpl_->deadman_switch >= 0, "TeleopTwistJoy", "Teleop deadman_switch %i.", pimpl_->deadman_switch);
 
   for (std::map<std::string, int>::iterator it = pimpl_->axis_linear_map.begin(); it != pimpl_->axis_linear_map.end(); ++it)
   {
     ROS_INFO_NAMED("TeleopTwistJoy", "Linear axis %s on %i", it->first.c_str(), it->second);
-    ROS_INFO_COND_NAMED(pimpl_->enable_turbo_button >= 0, "TeleopTwistJoy", "Turbo for linear axis %s", it->first.c_str());
+    // ROS_INFO_COND_NAMED(pimpl_->enable_turbo_button >= 0, "TeleopTwistJoy", "Turbo for linear axis %s", it->first.c_str());
   }
 
   for (std::map<std::string, int>::iterator it = pimpl_->axis_angular_map.begin(); it != pimpl_->axis_angular_map.end(); ++it)
   {
     ROS_INFO_NAMED("TeleopTwistJoy", "Angular axis %s on %i", it->first.c_str(), it->second);
-    ROS_INFO_COND_NAMED(pimpl_->enable_turbo_button >= 0, "TeleopTwistJoy", "Turbo for angular axis %s", it->first.c_str());
+    // ROS_INFO_COND_NAMED(pimpl_->enable_turbo_button >= 0, "TeleopTwistJoy", "Turbo for angular axis %s", it->first.c_str());
   }
 
   for (std::map<std::string, double>::iterator it = pimpl_->deadzone_linear_map.begin(); it != pimpl_->deadzone_linear_map.end(); ++it)
@@ -135,6 +141,8 @@ TeleopTwistJoy::TeleopTwistJoy(ros::NodeHandle* nh, ros::NodeHandle* nh_param)
   {
     ROS_INFO_NAMED("TeleopTwistJoy", "Angular deadzone of %s is %f", it->first.c_str(), it->second);
   }
+
+  ROS_INFO_NAMED("TeleopTwistJoy", "Teleop deadzone rotation %f.", pimpl_->deadzone_rotation);
 
   pimpl_->sent_disable_msg = false;
 
@@ -151,7 +159,7 @@ TeleopTwistJoy::TeleopTwistJoy(ros::NodeHandle* nh, ros::NodeHandle* nh_param)
 
 /**
  * @return the sign of the given value
-*/
+ */
 int sgn(double val) { return (val < 0) - (0 < val); }
 
 /**
@@ -159,6 +167,13 @@ int sgn(double val) { return (val < 0) - (0 < val); }
  * else returns adjusted to the deadzone new value
  */
 double ignoreDeadzone(double value, double deadzone) { return (std::abs(value) <= deadzone) ? 0.0 : value - deadzone * sgn(value); }
+
+double updateLinearXToRotationDeadzone(double lin_x_value, double ang_z_value, double deadzone_rotation)
+{
+  double interpolated_deadzone_rotation = deadzone_rotation * ang_z_value;
+
+  return (std::abs(lin_x_value) <= interpolated_deadzone_rotation) ? 0.0 : lin_x_value;
+}
 
 /**
  * @return value adjusted to the given deadzone and rescaled to a new value range according to the given min and max
@@ -172,7 +187,8 @@ double rescaleValue(double value, double min, double max, double deadzone)
 }
 
 double getVal(const sensor_msgs::Joy::ConstPtr& joy_msg, const std::map<std::string, int>& axis_map, const std::map<std::string, double>& deadzone_map,
-              const std::string& fieldname, const std::map<std::string, double>& min_map, const std::map<std::string, double>& max_map)
+              const std::string& fieldname, const std::map<std::string, double>& min_map, const std::map<std::string, double>& max_map, double deadzone_rotation = 0.0,
+              double yaw = 0.0)
 {
   if (axis_map.find(fieldname) == axis_map.end() || joy_msg->axes.size() <= axis_map.at(fieldname) || deadzone_map.find(fieldname) == deadzone_map.end() ||
       min_map.find(fieldname) == min_map.end() || max_map.find(fieldname) == max_map.end())
@@ -180,7 +196,14 @@ double getVal(const sensor_msgs::Joy::ConstPtr& joy_msg, const std::map<std::str
     return 0.0;
   }
 
-  return rescaleValue(joy_msg->axes[axis_map.at(fieldname)], min_map.at(fieldname), max_map.at(fieldname), deadzone_map.at(fieldname));
+  double value = joy_msg->axes[axis_map.at(fieldname)];
+
+  if (fieldname == "x")
+  {
+    value = updateLinearXToRotationDeadzone(value, yaw, deadzone_rotation);
+  }
+
+  return rescaleValue(value, min_map.at(fieldname), max_map.at(fieldname), deadzone_map.at(fieldname));
 }
 
 void TeleopTwistJoy::Impl::sendCmdVelMsg(const sensor_msgs::Joy::ConstPtr& joy_msg, const std::string& which_map)
@@ -188,7 +211,7 @@ void TeleopTwistJoy::Impl::sendCmdVelMsg(const sensor_msgs::Joy::ConstPtr& joy_m
   // Initializes with zeros by default.
   geometry_msgs::Twist cmd_vel_msg;
 
-  cmd_vel_msg.linear.x = getVal(joy_msg, axis_linear_map, deadzone_linear_map, "x", min_linear_map, max_linear_map);
+  cmd_vel_msg.linear.x = getVal(joy_msg, axis_linear_map, deadzone_linear_map, "x", min_linear_map, max_linear_map, deadzone_rotation, joy_msg->axes[axis_angular_map.at("yaw")]);
   cmd_vel_msg.linear.y = getVal(joy_msg, axis_linear_map, deadzone_linear_map, "y", min_linear_map, max_linear_map);
   cmd_vel_msg.linear.z = getVal(joy_msg, axis_linear_map, deadzone_linear_map, "z", min_linear_map, max_linear_map);
   // multiply the z angular velocity with the right factor for the robot to steer in the right direction
@@ -202,11 +225,12 @@ void TeleopTwistJoy::Impl::sendCmdVelMsg(const sensor_msgs::Joy::ConstPtr& joy_m
 
 void TeleopTwistJoy::Impl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
 {
-  if (!require_turbo_button || enable_turbo_button >= 0 && joy_msg->buttons.size() > enable_turbo_button && joy_msg->buttons[enable_turbo_button])
+  /*if (!require_turbo_button || enable_turbo_button >= 0 && joy_msg->buttons.size() > enable_turbo_button && joy_msg->buttons[enable_turbo_button])
   {
     sendCmdVelMsg(joy_msg, "turbo");
   }
-  else if (deadman_switch == -1 || joy_msg->buttons.size() > deadman_switch && joy_msg->buttons[deadman_switch])
+  else*/
+  if (deadman_switch == -1 || joy_msg->buttons.size() > deadman_switch && joy_msg->buttons[deadman_switch])
   {
     sendCmdVelMsg(joy_msg, "normal");
   }
